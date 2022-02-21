8c24f55e0592ebdf12c45424049fd399384d0c52
Tanvir <tanvir.roni1997@gmail.com>
2022-02-21 15:18:00 +0600

    First attempt to test the configuration of the repo

---
+++ sum_of_two_numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num_1, num_2, sum;
+
+    cout << "Enter two integer numbers for addition: " << endl;
+    cin >> num_1 >> num_2;
+
+    sum = num_1 + num_2;
+
+    cout << "Sum of two numbers is: " << sum << endl;
+
+    return 0;
+}
